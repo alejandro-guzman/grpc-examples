047e2ce746d6ea030e1305c9a61f755533572fe5
Alejandro Guzman <a.guillermoguzman@gmail.com>
2019-05-15 12:37:02 -0400

    testing service and proto

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # grpc-examples
+
+Install depnedencies
+
+gRPC
+
+```bash
+go get -u google.golang.org/grpc
+```
+
+Follow this link and download protoc compiler
+
+`https://github.com/google/protobuf/releases`
+
+Unzip and add to you path
+
+Install protoc plugin for GO
+
+```bash
+go get -u github.com/golang/protobuf/protoc-gen-go
+```
---
+++ echo-example/server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/agguzman/grpc-examples/echo-example"
+	grpc "google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) Echo(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: "echoing " + in.Message}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+	pb.RegisterEchoServer(srv, &server{})
+	if err := srv.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
